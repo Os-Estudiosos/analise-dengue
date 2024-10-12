157e304974b1c1b4b6e23451d30f07a8d88d4775
ThalisAFalqueto <thalisambrosimfalqueto@gmail.com>
2024-10-12 04:39:46 -0300

    Corrigindo a estrutura do programa principal

--- src/hypothesis_4.py
+++ src/hypothesis_4.py
@@ -7,7 +7,6 @@ from src.hypothesis.hypothesis_4 import hypothesis4
 from src.config import DATASET_LOCAL
 
 def main():
-   
     dataset = pd.read_csv(os.path.join(DATASET_LOCAL(), 'sinan_dengue_sample_2021.csv'), low_memory=False)
     hypothesis4(dataset)
 
