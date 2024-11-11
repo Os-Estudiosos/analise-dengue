00ff8022da152b6bd466b0655d70212a85d0172d
jaopredo <joao.profissional.45@gmail.com>
2024-11-11 15:56:58 -0300

    Código feito apenas para validar algumas hipóteses na concepção da nova hipótese 3

--- src/hypothesis/hypothesis_3.py
+++ src/hypothesis/hypothesis_3.py
@@ -9,76 +9,14 @@ from src.config import OUTPUT_FOLDER
 
 
 def hypothesis3(df: pd.DataFrame) -> None:
-    EXAMS = [
-        "RESUL_VI_N",  # RESULTADO DO EXAME VIRAL
-        "RESUL_NS1",  # RESULTADO DO EXAME NS1
-        "RESUL_PCR_",  # RESULTADO DO EXAME PCR
-        "RESUL_SORO", # RESUTLADO DO EXAME SOROLÓGICO
-    ]
+    AGE_COLUMN = 'NU_IDADE_N'  # Lista das colunas que vou utilizar
+    
+    # Retiro apenas as colunas necessárias
+    new_df = df[AGE_COLUMN]
 
-    SYMPTOMS = [
-        # PRESENÇA DE SINTOMAS:
-        "FEBRE",
-        "MIALGIA",
-        "CEFALEIA",
-        "EXANTEMA",
-        "VOMITO",
-        "NAUSEA",
-        "DOR_COSTAS",
-        "CONJUNTVIT",
-        "ARTRITE",
-        "ARTRALGIA",
-        "PETEQUIA_N",
-        "LEUCOPENIA",
-        "DOR_RETRO",
-        "DIABETES",
-        "HEMATOLOG",
-        "HEPATOPAT",
-        "RENAL",
-        "HIPERTENSA",
-        "ACIDO_PEPT",
-        "AUTO_IMUNE"
-    ]
-
-    # # Retira apenas as colunas necessárias
-    new_df = df[[*EXAMS, *SYMPTOMS]]
+    print(new_df[AGE_COLUMN[0]].value_counts())
 
     # Vou fazer tabelas e medir o Coeficiente de Contigência entre cada exame e cada teste,
     # assim eu filtro aqueles que tem relação com os que não tem nenhuma
-
-    contigency_table = pd.DataFrame(index=EXAMS, columns=SYMPTOMS, dtype=pd.Float32Dtype())  # Tabela que vai ter cada coeficiente de contigência entre cada exame e sintoma
-
-    def calcular_qui_quadrado(exam: str, symptom: str) -> dict:
-        """Function that calculate Square Chi between an exam and a symptom and returns a dictionary (Made to use with multithread)
-
-        Args:
-            exam (str): Exam
-            symptom (str): Symptom
-
-        Returns:
-            dict: Dictionary with both
-        """
-        ct = contigency_coefficient(new_df[exam], new_df[symptom])
-        return { 'exam': exam, 'symptom': symptom, 'ct': ct }
-        
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:  # Estrutura do multithreading
-        threads_running: list[concurrent.futures.Future] = []
-
-        for exam in EXAMS:
-            for symptom in SYMPTOMS:
-                threads_running.append(
-                    executor.submit(
-                        calcular_qui_quadrado,
-                        exam,
-                        symptom
-                    )
-                )
-
-        concurrent.futures.wait(threads_running)  # Esperando todas as tasks executarem
-
-        for pending in threads_running:
-            result = pending.result()
-            contigency_table.loc[result['exam'], result['symptom']] = result['ct']
     
-    contigency_table.to_csv(os.path.join(OUTPUT_FOLDER(), 'contigency_table_exams_and_symptoms.csv'))
+    # contigency_table = pd.DataFrame(index=COMPLICATIONS.keys(), columns=EVOLUTIONS.keys(), dtype=pd.Float32Dtype())
--- src/hypothesis_3.py
+++ src/hypothesis_3.py
@@ -1,16 +1,51 @@
 import pandas as pd
-
+import concurrent.futures
 import os
 import sys
 sys.path.append(os.getcwd())
 from src.hypothesis.hypothesis_3 import hypothesis3
-from src.config import DATASET_LOCAL
-from src.utils.reading import processing_total_dataset
+from src.config import *
+from src.filtering import filter_dataset
+from src.utils.timing import measure_function_execution
+# from src.utils.reading import processing_total_dataset
 
-def main():
-    dataset = processing_total_dataset()
-    hypothesis3(dataset)
+
+def hyp3_runner(chunk: pd.DataFrame):
+    df = filter_dataset(chunk)
+    # print(df)
+    hypothesis3(chunk)
+
+
+@measure_function_execution
+def main(year):
+    try:
+        # Vou ler o dataset por chunks
+        chunks = pd.read_csv(os.path.join(DATASET_LOCAL(), f'sinan_dengue_sample_{year}.csv'), low_memory=False, chunksize=CHUNKS_SIZE)
+        
+        # Carrego o arquivo com UF e acrônimos
+        cities = pd.read_csv(os.path.join(FILES_FOLDER(), "ufs.csv"), usecols=["SG_UF_NOT","SIGLA_UF"], low_memory=False)
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            threads_running: list[concurrent.futures.Future] = []
+
+            for chunk in chunks:  # Para cada chunk
+                merged_data = pd.merge(chunk, cities, on="SG_UF_NOT", how="left")
+                # Eu junto o dataset das siglas com o dataset original (Chunk)
+
+                threads_running.append(
+                    executor.submit(
+                        hyp3_runner,
+                        merged_data
+                    )
+                )
+
+            concurrent.futures.wait(threads_running)
+
+            for pending_thread in threads_running:
+                pending_thread.result()
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
-    main()
+    main(2024)
