bf80bb809b4b63bbccc85539b37749dbe721b0c1
jaopredo <joao.profissional.45@gmail.com>
2024-11-13 10:44:03 -0300

    Obtendo as informações que vou precisar para calcular as variâncias

--- src/hypothesis/hypothesis_3.py
+++ src/hypothesis/hypothesis_3.py
@@ -9,6 +9,7 @@ from src.filtering import filter_dataset
 
 def hypothesis3(chunks: TextFileReader) -> None:
     AGE_COLUMN = 'IDADE_ANOS'  # Coluna da idade que vou utilizar
+    SYMPTO_CLASSFICIATION = 'SINTOMA_CLASSIFIC'
     DANGER_SYMTOMPS_DICT = {  # Dicionário separando os sintomas em suas respectivas classificações
         'serious': [
             'RENAL',
@@ -38,7 +39,7 @@ def hypothesis3(chunks: TextFileReader) -> None:
         """Função resposável por pegar as informações que posteriormente usarei para calcular o R²
 
         Args:
-            chunk (pd.DataFrame): _description_
+            chunk (pd.DataFrame): Dataframe que eu vou pegar as informações
         """
         new_chunk = filter_dataset(chunk)[[  # Eu pego o chunk filtrado e utilizo apenas as colunas de sintomase  idade que eu defini anterioremente
             AGE_COLUMN,
@@ -47,32 +48,71 @@ def hypothesis3(chunks: TextFileReader) -> None:
             *DANGER_SYMTOMPS_DICT['serious'],
         ]]
 
+        informations_to_return = {
+            'common': {
+                'sum': 0,
+                'square_sum': 0,
+                'length': 0
+            },
+            'worrying': {
+                'sum': 0,
+                'square_sum': 0,
+                'length': 0
+            },
+            'serious': {
+                'sum': 0,
+                'square_sum': 0,
+                'length': 0
+            },
+        }
+
         # O Novo chunk que vou trabalhar que tem apenas a coluna de idades e uma que vai dizer a classificação dos sintomas do paciente
-        refactored_chunk = pd.DataFrame(new_chunk[AGE_COLUMN], columns=[AGE_COLUMN, 'SINTOMA_CLASSIFIC'])
+        refactored_chunk = pd.DataFrame(new_chunk[AGE_COLUMN], columns=[AGE_COLUMN, SYMPTO_CLASSFICIATION])
 
         # Vou analisar se a pessoa tem algum sintoma grave
         for symptom in DANGER_SYMTOMPS_DICT['serious']:
-            refactored_chunk['SINTOMA_CLASSIFIC'] = (refactored_chunk['SINTOMA_CLASSIFIC'] | (new_chunk[symptom]==1)).apply(
+            refactored_chunk[SYMPTO_CLASSFICIATION] = (new_chunk[symptom]==1).apply(
                 lambda x: 'GRAVE' if x else None  # Vai ter a classificação do sintoma como GRAVE se existir algum valor TRUE
             )
-        
+
         # Também vou analisar se tem sintomas preocupantes
         for symptom in DANGER_SYMTOMPS_DICT['worrying']:
             worrying_symtpoms_serie = (new_chunk[symptom]==1).apply(lambda x: 'PREOCUPANTE' if x else None)
-            refactored_chunk['SINTOMA_CLASSIFIC'] = refactored_chunk['SINTOMA_CLASSIFIC'].combine_first(worrying_symtpoms_serie)
+            refactored_chunk[SYMPTO_CLASSFICIATION] = refactored_chunk[SYMPTO_CLASSFICIATION].combine_first(worrying_symtpoms_serie)
             # Combino com a nova Série dos sintomas preocupantes, assim, eu classifico apenas aqueles que
             # Tem sintomas preocupantes mas não tem nenhum sintoma grave
         
 
         common_symtpoms_serie = pd.Series(np.tile(
             ['COMUM'],
-            len(refactored_chunk['SINTOMA_CLASSIFIC'])
+            len(refactored_chunk[SYMPTO_CLASSFICIATION])
         ))
-        refactored_chunk['SINTOMA_CLASSIFIC'] = refactored_chunk['SINTOMA_CLASSIFIC'].combine_first(common_symtpoms_serie)
+        refactored_chunk[SYMPTO_CLASSFICIATION] = refactored_chunk[SYMPTO_CLASSFICIATION].combine_first(common_symtpoms_serie)
         # O resto que não teve classificação, tem apenas sintomas comuns ou são assintomáticos, então eu crio
         # uma série que tem o tamanho do meu dataset e preencho todas as classificações sobrantes como COMUM
+
+        # Separando as séries de booleanos que usarei para filtrar os pacientes em suas respectivas classificações de sintomas
+        boolean_serious_classification_series = refactored_chunk[SYMPTO_CLASSFICIATION] == 'GRAVE'
+        boolean_worries_classification_series = refactored_chunk[SYMPTO_CLASSFICIATION] == 'PREOCUPANTE'
+        boolean_common_classification_series = refactored_chunk[SYMPTO_CLASSFICIATION] == 'COMUM'
         
-    
+        # Pegando quantos pacientes de cada classificação há
+        informations_to_return['serious']['length'] = len(refactored_chunk[boolean_serious_classification_series])
+        informations_to_return['worrying']['length'] = len(refactored_chunk[boolean_worries_classification_series])
+        informations_to_return['common']['length'] = len(refactored_chunk[boolean_common_classification_series])
+
+        # Pegando a soma das idades
+        informations_to_return['serious']['sum'] = (refactored_chunk[boolean_serious_classification_series])[AGE_COLUMN].sum()
+        informations_to_return['worrying']['sum'] = (refactored_chunk[boolean_worries_classification_series])[AGE_COLUMN].sum()
+        informations_to_return['common']['sum'] = (refactored_chunk[boolean_common_classification_series])[AGE_COLUMN].sum()
+
+        # Pegando a soma ao quadrado das idades
+        informations_to_return['serious']['square_sum'] = (refactored_chunk[boolean_serious_classification_series])[AGE_COLUMN].pow(2).sum()
+        informations_to_return['worrying']['square_sum'] = (refactored_chunk[boolean_worries_classification_series])[AGE_COLUMN].pow(2).sum()
+        informations_to_return['common']['square_sum'] = (refactored_chunk[boolean_common_classification_series])[AGE_COLUMN].pow(2).sum()
+
+        return informations_to_return
+
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
         threads_running: list[concurrent.futures.Future] = []
