122f50456abfe15ecd7e2cffeaff11e1d2ab6141
jaopredo <joao.profissional.45@gmail.com>
2024-11-14 10:48:24 -0300

    Refatorando com algumas funções padrão

--- src/hypothesis_3.py
+++ src/hypothesis_3.py
@@ -5,7 +5,6 @@ import sys
 sys.path.append(os.getcwd())
 from src.hypothesis.hypothesis_3 import hypothesis3
 from src.config import *
-from src.filtering import filter_dataset
 from src.utils.timing import measure_function_execution
 from src.utils.reading import processing_total_dataset
 
@@ -13,7 +12,7 @@ from src.utils.reading import processing_total_dataset
 @measure_function_execution
 def main():
     # Vou ler o dataset por chunks
-    chunks = pd.read_csv(os.path.join(DATASET_LOCAL(), f'sinan_dengue_sample_total.csv'), low_memory=False, chunksize=CHUNKS_SIZE)
+    chunks = processing_total_dataset(complete=False)
 
     hypothesis3(chunks)
 
